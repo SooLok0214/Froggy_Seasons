c35821bb9f072f2da013bfb912269341625fb3f1
SooLok0214 <wenglok241026@dasein.edu.my>
2026-08-08 05:05:00 +0800

    feat:done basic UI using

--- Assets/Script/GMScript/UIManager.cs
+++ Assets/Script/GMScript/UIManager.cs
@@ -1,16 +1,213 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Button pauseResumeBtn;
+    public Sprite pauseImg;
+    public Sprite resumeImg;
+
+    public GameObject pausePanel;
+    public GameObject startPanel;
+    public GameObject tapToStart;
+    public GameObject gameOverPanel;
+
+    public Button testGameStateBtn;
+
+    public bool gameStarted;
+
+    public GameObject pauseMenu;
+    public GameObject settingsPanel;
+    public GameObject menuBack;
+
     void Start()
     {
-        
+        Time.timeScale = 1;
+        gameStarted = false;
+
+        SetupSettingsMenu();
+
+        startPanel.SetActive(true);
+
+        pausePanel.SetActive(false);
+        pauseMenu.SetActive(false);
+        settingsPanel.SetActive(false);
+        menuBack.SetActive(false);
+
+        pauseResumeBtn.gameObject.SetActive(false);
+        pauseResumeBtn.interactable = false;
+
+        gameOverPanel.SetActive(false);
+
+        testGameStateBtn.gameObject.SetActive(true);
+        UpdateTestButtonLabel("TEST START");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetupSettingsMenu()
     {
-        
+        foreach (Button button in pausePanel.GetComponentsInChildren<Button>(true))
+        {
+            if (button.name.Contains("Setting"))
+            {
+                button.onClick.AddListener(OpenSettings);
+                break;
+            }
+        }
+
+        menuBack.GetComponent<Button>().onClick.AddListener(BackToPauseMenu);
+    }
+
+    public void OpenSettings()
+    {
+        if (!gameStarted)
+        {
+            return;
+        }
+
+        pauseMenu.SetActive(false);
+        settingsPanel.SetActive(true);
+        menuBack.SetActive(true);
+    }
+
+    public void BackToPauseMenu()
+    {
+        if (!gameStarted)
+        {
+            return;
+        }
+
+        ResetPauseMenu();
+    }
+
+    public void ResetPauseMenu()
+    {
+        pauseMenu.SetActive(true);
+        settingsPanel.SetActive(false);
+        menuBack.SetActive(false);
+    }
+
+    public void PauseResume()
+    {
+        if (!gameStarted)
+        {
+            return;
+        }
+
+        if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+
+            pauseResumeBtn.image.overrideSprite = pauseImg;
+
+            pausePanel.SetActive(false);
+            pauseMenu.SetActive(false);
+            settingsPanel.SetActive(false);
+            menuBack.SetActive(false);
+        }
+        else
+        {
+            Time.timeScale = 0;
+
+            pauseResumeBtn.image.overrideSprite = resumeImg;
+
+            pausePanel.SetActive(true);
+            ResetPauseMenu();
+        }
+    }
+
+    public void TestGameState()
+    {
+        if (!gameStarted)
+        {
+            StartGame();
+        }
+        else
+        {
+            GameOver();
+        }
+    }
+
+    public void StartGame()
+    {
+        gameStarted = true;
+        Time.timeScale = 1;
+
+        startPanel.SetActive(false);
+
+        if (tapToStart != null)
+        {
+            tapToStart.SetActive(false);
+        }
+
+        gameOverPanel.SetActive(false);
+
+        pausePanel.SetActive(false);
+        pauseMenu.SetActive(false);
+        settingsPanel.SetActive(false);
+        menuBack.SetActive(false);
+
+        pauseResumeBtn.image.overrideSprite = pauseImg;
+        pauseResumeBtn.gameObject.SetActive(true);
+        pauseResumeBtn.interactable = true;
+
+        testGameStateBtn.gameObject.SetActive(true);
+        UpdateTestButtonLabel("TEST DEATH");
+    }
+
+    public void GameOver()
+    {
+        gameStarted = false;
+        Time.timeScale = 0;
+
+        pausePanel.SetActive(false);
+        pauseMenu.SetActive(false);
+        settingsPanel.SetActive(false);
+        menuBack.SetActive(false);
+
+        pauseResumeBtn.image.overrideSprite = pauseImg;
+        pauseResumeBtn.gameObject.SetActive(true);
+        pauseResumeBtn.interactable = false;
+
+        gameOverPanel.SetActive(true);
+
+        UpdateTestButtonLabel("TEST START");
+        testGameStateBtn.gameObject.SetActive(false);
+    }
+
+    public void UpdateTestButtonLabel(string labelText)
+    {
+        Text label = testGameStateBtn.GetComponentInChildren<Text>();
+
+        if (label != null)
+        {
+            label.text = labelText;
+        }
+    }
+
+    public void BackToStartMenu()
+    {
+        gameStarted = false;
+        Time.timeScale = 1;
+
+        gameOverPanel.SetActive(false);
+
+        pausePanel.SetActive(false);
+        pauseMenu.SetActive(false);
+        settingsPanel.SetActive(false);
+        menuBack.SetActive(false);
+
+        pauseResumeBtn.image.overrideSprite = pauseImg;
+        pauseResumeBtn.gameObject.SetActive(false);
+        pauseResumeBtn.interactable = false;
+
+        startPanel.SetActive(true);
+
+        if (tapToStart != null)
+        {
+            tapToStart.SetActive(true);
+        }
+
+        testGameStateBtn.gameObject.SetActive(true);
+        UpdateTestButtonLabel("TEST START");
     }
 }
